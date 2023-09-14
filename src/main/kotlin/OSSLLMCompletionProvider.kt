ac1087da3534a0b770c563111fd173b3ab220ef6
Григорий <grisolnyshkin04@gmail.com>
2023-09-14 13:43:30 +0300

    now only the part of the document before the caret is sent to server testAction is also adjusted

--- src/main/kotlin/OSSLLMCompletionProvider.kt
+++ src/main/kotlin/OSSLLMCompletionProvider.kt
@@ -1,10 +1,10 @@
 package com.github.bzz.intellij
 
 import com.github.bzz.intellij.requests.Requester
-import com.google.gson.JsonSyntaxException
 import com.intellij.codeInsight.inline.completion.InlineCompletionElement
-import com.intellij.codeInsight.inline.completion.InlineCompletionProvider;
+import com.intellij.codeInsight.inline.completion.InlineCompletionProvider
 import com.intellij.codeInsight.inline.completion.InlineCompletionRequest
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.editor.event.DocumentEvent
 import java.io.IOException
@@ -13,7 +13,12 @@ class OSSLLMCompletionProvider : InlineCompletionProvider {
 
     override suspend fun getProposals(request: InlineCompletionRequest): List<InlineCompletionElement> {
         try {
-            val proposal = Requester.getModelSuggestions(request.document.text).text ?: return emptyList()
+            val caretPosition = runReadAction {
+                request.editor.caretModel.primaryCaret.selectionStart
+            }
+            val proposal = Requester.getModelSuggestions(
+                request.document.text.substring(0, caretPosition)
+            ).text ?: return emptyList()
             return listOf(InlineCompletionElement(proposal))
         } catch(exception: RuntimeException) {
             thisLogger().warn("Message: ${exception.message}")
@@ -21,8 +26,8 @@ class OSSLLMCompletionProvider : InlineCompletionProvider {
         } catch(ioexception: IOException) {
             thisLogger().warn("Message: ${ioexception.message}")
             return emptyList()
-        } catch(interrupted_exception: InterruptedException) {
-            thisLogger().warn("Message: ${interrupted_exception.message}")
+        } catch(interruptedException: InterruptedException) {
+            thisLogger().warn("Message: ${interruptedException.message}")
             return emptyList()
         }
     }
