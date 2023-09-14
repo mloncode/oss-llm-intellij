48134d1f3d9e3af067908adeca74eb59db5328c6
Григорий <grisolnyshkin04@gmail.com>
2023-09-15 01:52:34 +0300

    renewed server side according to spec added implementation to get a models list adjusted some namings improved loggers

--- src/main/kotlin/com/github/bzz/intellij/models/AvailableModels.kt
+++ src/main/kotlin/com/github/bzz/intellij/models/AvailableModels.kt
@@ -1,5 +1,7 @@
 package com.github.bzz.intellij.com.github.bzz.intellij.models
 
+import com.github.bzz.intellij.requests.Requester
+import com.intellij.openapi.diagnostic.Logger
 import java.util.concurrent.atomic.AtomicInteger
 
 
@@ -7,5 +9,22 @@ object AvailableModels {
 
     val currentModelIndex = AtomicInteger(0)
 
-    val modelsList: List<String> = listOf("proxy-model", "other proxy-model")
+    private val logger = Logger.getInstance("(getting available models)")
+
+    private fun getAvailableModels(): List<String> =
+        try {
+            Requester.getAvailableModels().models.map { it.modelName }
+        } catch (knownException: Requester.RequesterException) {
+            logger.warn(knownException.message)
+            emptyList()
+        } catch (otherException: Requester.RequesterException) {
+            logger.warn("Unknown exception. Message: ${otherException.message}")
+            emptyList()
+        }
+
+    val modelsList: List<String> = getAvailableModels()
+
+    fun currentModel() : String? {
+        return modelsList.getOrNull(currentModelIndex.get())
+    }
 }
--- src/main/kotlin/com/github/bzz/intellij/requests/ServerResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.github.bzz.intellij.com.github.bzz.intellij.requests
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ServerResponse (
-    val text: String?
-)
